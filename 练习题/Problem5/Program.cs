30f59b42e9b0d6380e1afe7767d83fd444bca920
Havemoresleep <elainasokawaiidesu@outlook.com>
2026-08-03 00:39:33 +0800

    做习题

---
+++ 练习题/Problem5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace Problem5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine() ?? "";
+            str = str.ToLower();
+            string[] parts = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int words = parts.Length;
+            int[] counts = new int[26];
+            int letters = 0;
+            int maxCount = 0;
+            int maxIndex = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLetter(str[i]))
+                {
+                    char c = str[i];
+                    int index = c - 'a';
+                    counts[index]++;
+                }
+            }
+            for (int i = 0;i < counts.Length; i++)
+            {
+                letters += counts[i];
+                if (counts[i] > maxCount)
+                {
+                    maxCount = counts[i];
+                    maxIndex = i;
+                }
+            }
+            char answer = (char)(maxIndex + 'a');
+            Console.WriteLine($"字母数:{letters}");
+            Console.WriteLine($"单词数: {words}");
+            Console.WriteLine($"出现次数最多的字母: {answer}");
+
+        }
+    }
+}
---
+++ 练习题/Problem6/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Problem6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 1, 4, 1, 5 };
+            int[] result = new int[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int current = arr[i];
+                bool isExist = false;
+
+                for (int j = 0; j < result.Length; j++)
+                {
+                    if (result[j] == current)
+                    {
+                        isExist = true;
+                        break;
+                    }
+                }
+
+                if (!isExist)
+                {
+                    int[] newResult = new int[result.Length + 1];
+
+                    for (int k = 0; k < result.Length; k++)
+                    {
+                        newResult[k] = result[k];
+                    }
+
+                    newResult[result.Length] = current;
+
+                    result = newResult;
+                }
+            }
+
+            foreach (int n in result)
+                Console.Write(n + " ");
+        }
+    }
+}
